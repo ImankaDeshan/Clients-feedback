24c1e07edfffc6ec64dfc38575ebe3934d4b4179
Imanka Deshan <imankadeshan2000@gmail.com>
2025-04-25 21:18:16 +0530

    add assests

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,9 +9,6 @@ function App() {
       <div>
         
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
